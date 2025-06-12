009e1e513deceab64846d65d21565eb0be71dd1e
CarlosJChileS <carloschilesilva@gmail.com>
2025-06-12 18:06:21 -0500

    cambiar start.sh y

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# 1. Activa entorno virtual
-source ./aienv/bin/activate
+# 1. Inicia backend Python usando el Python del entorno virtual, en segundo plano
+./aienv/bin/python -m uvicorn playground:app --host 0.0.0.0 --port 7777 &
 
-# 2. Inicia backend Python en 7777 (background)
-uvicorn playground:app --host 0.0.0.0 --port 7777 &
-
-# 3. Inicia servidor Nginx (foreground)
+# 2. Inicia Nginx como proceso principal (foreground)
 nginx -g 'daemon off;'
