cff9627ab8f465f5444e9e0d7f222d55c5ce3d45
CarlosJChileS <carloschilesilva@gmail.com>
2025-06-12 19:57:20 -0500

    aver

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
-# 1. Inicia backend Python usando el Python del entorno virtual, en segundo plano
+# 1. Inicia backend Python usando el entorno virtual (background)
 ./aienv/bin/python -m uvicorn playground:app --host 0.0.0.0 --port 7777 &
 
-# 2. Inicia Nginx como proceso principal (foreground)
+# 2. Inicia Nginx como proceso principal
 nginx -g 'daemon off;'
